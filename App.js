b474f1bc7afb2f1a42d017170a49210981f97c12
supermantoman <jwg7632@gmail.com>
2023-02-19 19:13:48 +0900

    [Feat] 로고 SVG파일 업로드 및 마크업 완료

--- App.js
+++ App.js
@@ -7,17 +7,27 @@ const bgImage = require('./public/images/background-image.png')
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* StatusBar */}
       <StatusBar style="light" />
-      <ImageBackground source={bgImage} style={styles.image}>
-      </ImageBackground>      
-    </View>
 
+      {/* 백그라운드 이미지 */}
+      <View style={styles.image}>
+        <ImageBackground source={bgImage} style={styles.image} />
+      </View>
+
+      {/* 메인카피 */}
       <View style={styles.mainCopy}>
         <Text style={styles.mainCopy}>
           매일{'\n'}
           우아해질{'\n'}
           당신을 위해
         </Text>
+      </View>
+
+      {/* 로고 */}
+      <View style={styles.logo}>
+        <Logo style={styles.logo}></Logo>
+      </View>
     </View>
     
   );
@@ -27,23 +37,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
+    position: 'relative',
     // flexDirection: 'row',
   },
   image: {
     flex: 1,
     resizeMode: 'contain',
+    opacity: 0.8,
   },
   mainCopy: {
     flex: 1,
     color: '#fff',
-    fontSize: '40',
-    lineHeight: '48', // 적절한 높이로 조정
+    fontSize: '32',
+    lineHeight: '40', // 적절한 높이로 조정
     fontWeight: 'bold',
     position: 'absolute',
     marginTop: 76,
     marginLeft: 24,
   },
-  logoContainer: {
+  logo: {
     flex: 1,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: -25,
+    marginTop: -25,
   }
 });
