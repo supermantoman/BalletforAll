a93e757ff945814ba13273dc15fe7bdd508cba6d
supermantoman <jwg7632@gmail.com>
2023-02-19 15:15:33 +0900

    리액트 네이티브 초기 세팅 완료

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const bgImage = require('./assets/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <ImageBackground source={bgImage} style={styles.image}>
+        <Text style={styles.mainCopy}>
+          매일{'\n'}
+          우아해질{'\n'}
+          당신을 위해
+        </Text>
+      </ImageBackground>
+      <View></View>
+      <View></View>
     </View>
   );
 }
@@ -13,8 +23,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    // flexDirection: 'row',
   },
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
+  mainCopy: {
+    color: '#fff',
+    fontSize: '36',
+    lineHeight: '44', // 적절한 높이로 조정
+    fontWeight: 'bold',
+  }
 });
