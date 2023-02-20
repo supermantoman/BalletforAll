81ee06bfe82fa174b77577ae40bbe7fdfda60b06
supermantoman <jwg7632@gmail.com>
2023-02-20 17:51:07 +0900

    [feat] 스플래쉬 화면 마크업 수정

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -4,24 +4,24 @@ import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Logo from '../../../assets/logotypes.svg';
-import { SplashStyles } from './SplashScreenStyles';
+import { SplashStyles } from '../SplashScreen/SplashStyles';
 
 const bgImage = require('../../../public/images/background-image.png');
 
 export default function SplashScreen() {
   return (
-    <View style={styles.container}>
+    <View style={SplashStyles.container}>
       {/* StatusBar */}
       <StatusBar style="light" />
 
       {/* 백그라운드 이미지 */}
-      <View style={styles.image}>
-        <ImageBackground source={bgImage} style={styles.image} />
+      <View style={SplashStyles.image}>
+        <ImageBackground source={bgImage} style={SplashStyles.image} />
       </View>
 
       {/* 메인카피 */}
-      <View style={styles.mainCopy}>
-        <Text style={styles.mainCopy}>
+      <View style={SplashStyles.mainCopy}>
+        <Text style={SplashStyles.mainCopy}>
           매일{'\n'}
           우아해질{'\n'}
           당신을 위해
@@ -29,42 +29,10 @@ export default function SplashScreen() {
       </View>
 
       {/* 로고 */}
-      <View style={styles.logo}>
-        <Logo style={styles.logo}></Logo>
+      <View style={SplashStyles.logo}>
+        <Logo style={SplashStyles.logo}></Logo>
       </View>
     </View>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    position: 'relative',
-    // flexDirection: 'row',
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'contain',
-    opacity: 0.8,
-  },
-  mainCopy: {
-    flex: 1,
-    color: '#fff',
-    fontSize: '36',
-    lineHeight: '40', // 적절한 높이로 조정
-    fontWeight: 'bold',
-    position: 'absolute',
-    marginTop: 76,
-    marginLeft: 24,
-  },
-  logo: {
-    flex: 1,
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginLeft: -25,
-    marginTop: -25,
-  }
-});
