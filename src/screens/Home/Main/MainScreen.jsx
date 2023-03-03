5b72a29b2d4e9b04faecfeed77d2bfc21d53653d
supermantoman <jwg7632@gmail.com>
2023-03-04 02:55:56 +0900

    [feat] 메인페이지 listItem 마크업 및 스타일링 추가

--- src/screens/Home/Main/MainScreen.jsx
+++ src/screens/Home/Main/MainScreen.jsx
@@ -9,8 +9,6 @@ export default function MainScreen (){
       <StatusBar style="light" />
       <ScrollView>
         {/* 배너 영역 */}
-        {/* 내 위치 설정 및 필터영역 */}
-        {/* 학원 리스트 */}
         {/* 유튜브 영상 */}
         {/* 푸터 */}
         <View >
@@ -26,7 +24,68 @@ export default function MainScreen (){
             </Text>
           </View>
         </View>
-        <View></View>
+
+        {/* 내 위치 설정 및 필터영역 */}
+        <View style={MainScreenStyles.filterWrap}>
+          <Text style={MainScreenStyles.addressText}>서초 1동</Text>
+          <View style={MainScreenStyles.flitering}>
+            <Text>가까운 순</Text>
+          </View>
+        </View>
+
+        {/* 학원 리스트 */}
+        <View style={MainScreenStyles.listItem}>
+          <Image style={MainScreenStyles.itemImage} source={require('../../../assets/images/itemImage.png')} />
+          <View style={[MainScreenStyles.infoWrap, {backgroundColor:'#fff'}]}>
+            <View style={MainScreenStyles.titleWrap}>
+              <Text style={{fontSize:20, fontWeight: 'bold'}}>이화본발레</Text>
+              <Image style={{width:25, height:25}} source={require('../../../assets/icons/productHeartIcon.png')}></Image>
+          </View>
+          <Text style={{fontSize:14, marginTop:8, marginBottom:20, color: '#999'}}>서울시 영등포구 영등포동4가 123 가나빌딩 4층</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>정규수업 1회당 50,000원~</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>쿠폰수업 1회당 60,000원~</Text>
+          <Text style={{fontWeight:'400', }}>작품반 백조의 호수, 르네의 눈몰 외 2개</Text>
+          </View>
+        </View>
+        <View style={MainScreenStyles.listItem}>
+          <Image style={MainScreenStyles.itemImage} source={require('../../../assets/images/itemImage.png')} />
+          <View style={[MainScreenStyles.infoWrap, {backgroundColor:'#fff'}]}>
+            <View style={MainScreenStyles.titleWrap}>
+              <Text style={{fontSize:20, fontWeight: 'bold'}}>이화본발레</Text>
+              <Image style={{width:25, height:25}} source={require('../../../assets/icons/productHeartIcon.png')}></Image>
+          </View>
+          <Text style={{fontSize:14, marginTop:8, marginBottom:20, color: '#999'}}>서울시 영등포구 영등포동4가 123 가나빌딩 4층</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>정규수업 1회당 50,000원~</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>쿠폰수업 1회당 60,000원~</Text>
+          <Text style={{fontWeight:'400', }}>작품반 백조의 호수, 르네의 눈몰 외 2개</Text>
+          </View>
+        </View>
+        <View style={MainScreenStyles.listItem}>
+          <Image style={MainScreenStyles.itemImage} source={require('../../../assets/images/itemImage.png')} />
+          <View style={[MainScreenStyles.infoWrap, {backgroundColor:'#fff'}]}>
+            <View style={MainScreenStyles.titleWrap}>
+              <Text style={{fontSize:20, fontWeight: 'bold'}}>이화본발레</Text>
+              <Image style={{width:25, height:25}} source={require('../../../assets/icons/productHeartIcon.png')}></Image>
+          </View>
+          <Text style={{fontSize:14, marginTop:8, marginBottom:20, color: '#999'}}>서울시 영등포구 영등포동4가 123 가나빌딩 4층</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>정규수업 1회당 50,000원~</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>쿠폰수업 1회당 60,000원~</Text>
+          <Text style={{fontWeight:'400', }}>작품반 백조의 호수, 르네의 눈몰 외 2개</Text>
+          </View>
+        </View>
+        <View style={MainScreenStyles.listItem}>
+          <Image style={MainScreenStyles.itemImage} source={require('../../../assets/images/itemImage.png')} />
+          <View style={[MainScreenStyles.infoWrap, {backgroundColor:'#fff'}]}>
+            <View style={MainScreenStyles.titleWrap}>
+              <Text style={{fontSize:20, fontWeight: 'bold'}}>이화본발레</Text>
+              <Image style={{width:25, height:25}} source={require('../../../assets/icons/productHeartIcon.png')}></Image>
+          </View>
+          <Text style={{fontSize:14, marginTop:8, marginBottom:20, color: '#999'}}>서울시 영등포구 영등포동4가 123 가나빌딩 4층</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>정규수업 1회당 50,000원~</Text>
+          <Text style={{fontWeight:'400', marginBottom:12}}>쿠폰수업 1회당 60,000원~</Text>
+          <Text style={{fontWeight:'400', }}>작품반 백조의 호수, 르네의 눈몰 외 2개</Text>
+          </View>
+        </View>
         <View></View>
         <View></View>
       </ScrollView>
--- src/screens/Home/Main/MainScreenStyles.jsx
+++ src/screens/Home/Main/MainScreenStyles.jsx
@@ -3,6 +3,7 @@ import { Dimensions, StyleSheet } from 'react-native';
 export const MainScreenStyles = StyleSheet.create({
   container: {
     flex:1,
+    backgroundColor: '#ccc'
   },
   bgImage: {
     flex: 1,
@@ -13,22 +14,64 @@ export const MainScreenStyles = StyleSheet.create({
     flexDirection: 'column',
     position: 'absolute',
   },
+  filterWrap:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 24,
+    marginTop: 22,
+  },
+  addressText:{
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
   bannerText: {
     fontSize: 40,
     color: '#fff',
     fontWeight: 'bold',
     letterSpacing: 3,
-    paddingTop: 170,
+    paddingTop: 150,
     paddingLeft: 48,
     lineHeight: 48
   },
   bannerSubText:{
     fontSize: 16,
     color: '#ccc',
+    fontWeight: '500',
     letterSpacing: 2,
     paddingLeft: 48,
     marginTop: 16,
     lineHeight: 28,
-  }
+  },
+  infoWrap:{
+    position: 'absolute',
+    bottom: 0,
+    height: 190,
+    width: 327,
+    paddingHorizontal: 24,
+    paddingVertical: 24,
+  },
+  titleWrap:{
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  listItem :{
+    backgroundColor: '#fff',
+    height: 376,
+    marginTop: 24,
+    position: 'relative',
+    alignItems: 'center',
+    marginHorizontal: 24,
+  },
+  itemImage:{
+    resizeMode: 'cover',
+    height: '50%',
+    maxWidth: '50%',
+    position: 'absolute',
+  },
+  filtering:{
+    backgroundColor: 'blue',
+  },
 });
 
